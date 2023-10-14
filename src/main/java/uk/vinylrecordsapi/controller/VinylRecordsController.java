1a0885ed659a9ccf03c45d49b20e324ab49b70d0
Sam Thompson <66477261+samrjthompson@users.noreply.github.com>
2023-10-15 00:21:28 +0100

    Implement POST endpoint (#4)

--- src/main/java/uk/vinylrecordsapi/controller/VinylRecordsController.java
+++ src/main/java/uk/vinylrecordsapi/controller/VinylRecordsController.java
@@ -4,7 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import uk.vinylrecordsapi.model.request.VinylRecordRequest;
 import uk.vinylrecordsapi.model.response.VinylRecordResponse;
 import uk.vinylrecordsapi.service.VinylRecordsService;
 
@@ -21,8 +24,15 @@ public class VinylRecordsController {
     }
 
     @GetMapping("/vinyl_records")
-    public ResponseEntity<List<VinylRecordResponse>> getAllClientProfiles() {
+    public ResponseEntity<List<VinylRecordResponse>> getAllVinylRecords() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(service.getAllVinylRecords());
+    }
+
+    @PostMapping("/vinyl_records")
+    public ResponseEntity<Void> insertNewVinylRecord(
+            @RequestBody VinylRecordRequest request) {
+        service.insertNewVinylRecord(request);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
